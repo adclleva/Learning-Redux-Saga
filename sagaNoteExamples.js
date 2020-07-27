9360111cb9a503746109d13c2c70be0f95021f15
adclleva <arvin.lleva@yahoo.com>
2020-07-26 19:26:36 -0700

    Added the saganoteexamples

---
+++ sagaNoteExamples.js
@@ -0,0 +1,37 @@
+import { takeEvery, take, put, call } from "redux-saga/effects"; // these are helpers
+
+import { IMAGES } from "../constants";
+
+function* handleImagesLoad() {
+  console.log("fetching images from unsplash");
+}
+
+// watcher saga
+function* rootSaga() {
+  // every time the dispatch LOAD action is called, the handleImagesLoad runs
+  yield takeEvery(IMAGES.LOAD, handleImagesLoad); // takeEvery is considered non-blocking
+}
+
+// watcher saga => actions => worker saga
+export default rootSaga;
+
+// watcher saga
+// function* rootSaga() {
+// every time an action of 'HELLO' is dispatched, we run this workerSaga generator once with take
+// yield take('HELLO', workerSaga);
+// these are considered blocking
+// yield take('LOGIN');
+// yield call(workerSaga);
+// yield take('LOGOUT');
+// yield call(byebyeSaga);
+// }
+
+// function* workerSaga() {
+// console.log('Hey from worker');
+// console.log(put({ type: 'ACTION_FROM_WORKER' }));
+// yield put({ type: 'ACTION_FROM_WORKER' }); // we can use put to dispatch an action
+// }
+
+// function* byebyeSaga() {
+//     console.log('bye bye');
+// }
